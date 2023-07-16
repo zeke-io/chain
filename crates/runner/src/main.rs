0c5ddc30c843950a59bb92c945b20a41a1067bca
zeke-io <ezequiel_io@pm.me>
2023-07-16 16:19:39 -0300

    Implement server runner

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Child, Command, exit, Stdio};
+use common::{from_path, ServerMetadata};
+
+fn main() -> anyhow::Result<()> {
+    let metadata = from_path("./mcs.toml")
+        .expect("Cannot load metadata file");
+
+    run_server(metadata).wait()?;
+
+    Ok(())
+}
+
+fn run_server(metadata: ServerMetadata) -> Child {
+    let mut command = Command::new(&metadata.runtime.java_path);
+    command.current_dir("./");
+
+    for arg in metadata.runtime.jvm_options {
+        command.arg(arg);
+    }
+
+    command.arg("-jar");
+    command.arg(metadata.runtime.server_jar);
+
+    for arg in metadata.runtime.server_args {
+        command.arg(arg);
+    }
+
+    command
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap()
 }
