b6ede58fa9ef8f1c134f7a29f85ce63c51c3e652
zeke-io <ezequiel_io@pm.me>
2023-07-16 18:18:10 -0300

    Add function `from folder` to metadata reader

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs;
+use std::path::Path;
 use toml::Value;
 
 #[derive(Deserialize, Debug, Clone)]
@@ -24,6 +25,18 @@ pub struct ServerMetadata {
 
 pub fn from_path(path: &str) -> Option<ServerMetadata> {
     let contents = fs::read_to_string(path);
+
+    if let Ok(contents) = contents {
+        let metadata: ServerMetadata = toml::from_str(&contents).unwrap();
+        return Option::from(metadata);
+    }
+
+    None
+}
+
+pub fn from_folder(path: &str) -> Option<ServerMetadata> {
+    let file_path = Path::new(path).join("mcs.toml");
+    let contents = fs::read_to_string(file_path);
 
     if let Ok(contents) = contents {
         let metadata: ServerMetadata = toml::from_str(&contents).unwrap();
