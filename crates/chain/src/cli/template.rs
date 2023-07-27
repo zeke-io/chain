2ded0ebda37d61c531665413aff86a908ecfb573
zeke-io <ezequiel_io@pm.me>
2023-07-27 14:25:35 -0300

    Generate chain.yml file on init

--- crates/chain/src/cli/template.rs
+++ crates/chain/src/cli/template.rs
@@ -1,13 +1,32 @@
+use anyhow::Context;
+use chain::project::ProjectDetails;
+use inquire::Text;
+use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
 pub fn generate_template(path: PathBuf) -> anyhow::Result<()> {
+    let server_name = Text::new("What's the name of the server?").prompt()?;
+    let server_jar = Text::new("Provide the path or download url for the server jar:").prompt()?;
+
+    let project = ProjectDetails {
+        name: server_name,
+        server_jar,
+        dependencies: Default::default(),
+    };
+
+    generate_project_file(&path, project)?;
     generate_gitignore(&path)?;
     Ok(())
 }
 
-fn generate_gitignore(path: &PathBuf) -> anyhow::Result<()> {
+fn generate_project_file(directory: &PathBuf, project: ProjectDetails) -> anyhow::Result<()> {
+    let parsed = serde_yaml::to_string(&project)?;
+    fs::write(directory.join("chain.yml"), parsed).context("Could not create \"chain.yml\" file")
+}
+
+fn generate_gitignore(directory: &PathBuf) -> anyhow::Result<()> {
     let content = r#"### Chain
 .chain/
 
@@ -18,7 +37,7 @@ settings.dev.yml
 server/
 "#;
 
-    let mut file = File::create(path.join("../../../../.gitignore"))?;
+    let mut file = File::create(directory.join(".gitignore"))?;
     file.write_all(content.as_bytes())?;
     Ok(())
 }
