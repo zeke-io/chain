56f3ee4518274c2bb32dd4df4a676bc845ea893c
zeke-io <ezequiel_io@pm.me>
2023-07-27 14:00:08 -0300

    Add `init` command

---
+++ crates/cli/src/template.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+pub fn generate_template(path: PathBuf) -> anyhow::Result<()> {
+    generate_gitignore(&path)?;
+    Ok(())
+}
+
+fn generate_gitignore(path: &PathBuf) -> anyhow::Result<()> {
+    let content = r#"### Chain
+.chain/
+
+# Settings
+settings.dev.yml
+
+# Server
+server/
+"#;
+
+    let mut file = File::create(path.join(".gitignore"))?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
