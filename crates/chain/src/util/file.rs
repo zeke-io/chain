3b5e133768034101e2acf7b756ad4b792a76040d
zeke-io <ezequiel_io@pm.me>
2023-08-04 00:30:27 -0300

    Update `is_binary` function

--- crates/chain/src/util/file.rs
+++ crates/chain/src/util/file.rs
@@ -42,18 +42,35 @@ pub fn find_up_file(path: &Path, file_name: &str) -> Option<PathBuf> {
     }
 }
 
-/// Hacky way to know if a file is a binary (might not be accurate)
-pub fn is_binary<P: AsRef<Path>>(path: P) -> anyhow::Result<bool> {
+/// Detects if a file is most likely a binary based on the presence of null bytes and control characters.
+///
+/// It reads the first few bytes of the file
+/// to analyze its content and checks for the occurrence of null bytes (0x00)
+/// and extended ASCII control characters (0x00 to 0x1F and 0x7F).
+///
+/// If any byte that satisfies these conditions is found, the function returns `true`,
+/// indicating that the file is likely binary.
+///
+/// # Arguments
+///
+/// * `file_path` - The path to the file to be analyzed.
+///
+/// # Returns
+///
+/// * `Ok(true)` - If the file is likely binary.
+/// * `Ok(false)` - If the file is likely not binary (i.e., it's probably a text file).
+/// * `Err(io::Error)` - If there's an error reading the file.
+pub fn is_binary<P: AsRef<Path>>(file_path: P) -> anyhow::Result<bool> {
     const BYTES_TO_CHECK: usize = 256;
     let mut buffer = [0; BYTES_TO_CHECK];
-    let mut file = File::open(path)?;
+    let mut file = File::open(file_path)?;
 
-    let num_bytes = file.read(&mut buffer)?;
-    for &byte in buffer[..num_bytes].iter() {
-        if byte == 0 {
-            return Ok(true);
-        }
-    }
+    let bytes_read = file.read(&mut buffer)?;
+    let total_bytes = bytes_read.min(BYTES_TO_CHECK);
 
-    Ok(false)
+    let is_binary = buffer[..total_bytes]
+        .iter()
+        .any(|&byte| byte == 0 || byte <= 0x1F || byte == 0x7F);
+
+    Ok(is_binary)
 }
