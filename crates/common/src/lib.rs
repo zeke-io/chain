bfe79318d39a1799308cac1bad3f63e6811b4f85
zeke-io <ezequiel_io@pm.me>
2024-07-14 16:29:04 -0300

    Add `common` crate

---
+++ crates/common/src/lib.rs
@@ -0,0 +1,22 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub struct CraftyError {
+    message: String,
+}
+
+impl CraftyError {
+    pub fn new<T: Into<String>>(message: T) -> CraftyError {
+        CraftyError {
+            message: message.into(),
+        }
+    }
+}
+
+impl Display for CraftyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+pub type Result<T> = core::result::Result<T, CraftyError>;
